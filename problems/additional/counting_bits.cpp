3620e4035c3ec632635653a180a204f7cbb0f8ee
lintopher0315 <lin978@purdue.edu>
2020-07-19 21:56:51 -0400

    edited counting_bits

--- problems/additional/counting_bits.cpp
+++ problems/additional/counting_bits.cpp
@@ -6,8 +6,7 @@ int main() {
     long long n, t=0, p=1;
     cin >> n;
     while (p<=n) {
-        t+=((n+1)/(2*p)*p);
-        t+=max(0LL, (n+1)%(2*p)-p);
+        t+=((n+1)/(2*p)*p)+max(0LL, (n+1)%(2*p)-p);
         p<<=1;
     }
     cout << t;
